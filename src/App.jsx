b6c5eaba9cc296e5a89202f61bbc645b6fe83e58
[Aman Yadav] <[aman4864y@gmail.com]>
2024-06-26 10:53:02 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef} from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 
@@ -8,9 +8,7 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("")
 
-//useref hook
-const passwordRef = useRef(null)
-
+  const passref=useRef(null)
 
   const passwordGenerator = useCallback(()=>{
     let pass=""
@@ -19,24 +17,26 @@ const passwordRef = useRef(null)
     if(charAllowed) str+="`~!@#$%^&*()_+[]{}"
 
     for (let i = 1; i < length; i++) {
-      let char = Math.floor(Math.random()*str.length+1)
+      let char= Math.floor(Math.random()*str.length+1)
       pass += str.charAt(char)
 
     }
     setPassword(pass)
 
-  }, [length, numberAllowed, charAllowed, setPassword])
+  },  [length, numberAllowed, charAllowed, setPassword])
+  
 
-  const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select();
-    
-    window.navigator.clipboard.writeText(password)
-  },
-[password])
-
-  useEffect(() => {
+  useEffect(()=>{
     passwordGenerator()
-  } , [length, numberAllowed, charAllowed, passwordGenerator])
+
+  },[length,charAllowed,numberAllowed,passwordGenerator])
+
+const copyPassToClip = ()=>{
+  passref.current?.select();
+  window.navigator.clipboard.writeText(password)
+}
+
+
   return (
     <>
     <div className='w-full max-w-md mx-auto shadow-md 
@@ -51,12 +51,10 @@ const passwordRef = useRef(null)
           className='outline-none w-full py-1 px-3'
           placeholder='Password'
           readOnly
-          ref={passwordRef}
+          ref={passref}
         />
-        <button 
-        onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700
-         text-white px-3 py-0.5 shrink-0'>
+        <button className=' outline-none bg-blue-600
+         text-white px-3 py-0.5 shrink-0 hover:bg-blue-900 ' onClick={copyPassToClip} >
          Copy</button>
 
       </div>
@@ -64,8 +62,8 @@ const passwordRef = useRef(null)
         <div className='flex items-center gap-x-1'>
           <input 
           type="range"
-          min={6}
-          max={100}
+          min={8}
+          max={20}
           value={length}
           className='cursor-pointer'
           onChange={(e)=>{setLength(e.target.value)}}
